d9b65f5b710e43c65b7a9912c4368eb618da4465
Deepankar20 <deepankardt2012@gmail.com>
2024-08-25 21:29:30 +0530

    project complete

--- src/app/flipcoin/page.tsx
+++ src/app/flipcoin/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { useEffect, useState } from "react";
 import { Contract, ethers } from "ethers";
+import Image from "next/image";
 
 const FlipCoin = () => {
   const [betAmount, setBetAmount] = useState("");
@@ -9,6 +10,8 @@ const FlipCoin = () => {
   const providerUrl = process.env.NEXT_PUBLIC_ALCHEMY_SEPOLIA_URL;
   const [account, setAccount] = useState<string | null>(null);
   const [contract, setContract] = useState<Contract | null>(null);
+  const [won, setWon] = useState("");
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     async function func() {
@@ -46,9 +49,8 @@ const FlipCoin = () => {
       //@ts-ignore
       const listener = (player, amount, choice, won) => {
         console.log("Event received:", player, amount, choice, won);
-        if (player.toLowerCase() === account.toLowerCase()) {
-          setResult(won ? "You won the coin flip!" : "You lost the coin flip.");
-        }
+        setLoading(false);
+        setWon(won ? "true" : "false");
       };
 
       contract.on("CoinFlipped", listener);
@@ -139,6 +141,9 @@ const FlipCoin = () => {
   const flipCoin = async () => {
     if (providerUrl && side) {
       try {
+        setLoading(true);
+        setBetAmount("");
+        setSide("");
         const sideVal = side === "Heads" ? 0 : 1;
 
         const tx = await contract?.flipCoin(sideVal, {
@@ -155,25 +160,44 @@ const FlipCoin = () => {
 
   return (
     <div className="">
-      <div className="flex flex-col justify-center mx-auto">
+      <div className="flex flex-col justify-center mx-auto w-1/2 my-[10rem] gap-8 items-center">
         <input
           type="text"
           placeholder="Bet Amount (ETH)"
           value={betAmount}
           onChange={(e) => setBetAmount(e.target.value)}
-          className="text-black p-4"
+          className="text-black p-4 rounded-lg w-[30rem]"
         />
         <select
           value={side}
           onChange={(e) => setSide(e.target.value)}
-          className="bg-black"
+          className="bg-black border border-white p-4 rounded-lg  w-[30rem]"
         >
           <option value="">Select Side</option>
           <option value="Heads">Heads</option>
           <option value="Tails">Tails</option>
         </select>
-        <button onClick={flipCoin}>Flip Coin</button>
-        <p>{result}</p>
+        <button
+          onClick={flipCoin}
+          className="text-2xl text-black bg-white font-semibold border border-white rounded-xl p-2 w-[20rem] hover:bg-black hover:text-white"
+        >
+          Flip Coin
+        </button>
+        {loading && (
+          <Image
+            src={"https://i.giphy.com/3o7bu3XilJ5BOiSGic.webp"}
+            alt=""
+            width={50}
+            height={50}
+          />
+        )}
+        {won && (
+          <div
+            className={`${won == "true" ? "text-green-500" : "text-red-500"}`}
+          >
+            {won == "true" ? "You Won" : "You Lost"}
+          </div>
+        )}
       </div>
     </div>
   );
